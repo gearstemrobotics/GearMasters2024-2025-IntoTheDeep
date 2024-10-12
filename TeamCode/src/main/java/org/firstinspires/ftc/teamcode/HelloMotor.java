9784fc6fa3d3b38fa4bcbd0d89b1b013c98328b9
GearMasters <gearclubnewmexico@gmail.com>
2024-10-12 15:14:11 -0600

    updated the project and added HelloMotor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloMotor.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp()
+public class HelloMotor extends OpMode {
+
+    private DcMotor rightMotor;
+    @Override
+    public void init()
+    {
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        float power = 0;
+        if (gamepad1.left_trigger > 0) // if left trigger > 0
+        {
+            power = gamepad1.left_trigger;
+        }
+        else // check rtrigger
+        {
+            power = -gamepad1.right_trigger;
+        }
+        rightMotor.setPower(power);
+        telemetry.addData("power", power);
+    }
+}
