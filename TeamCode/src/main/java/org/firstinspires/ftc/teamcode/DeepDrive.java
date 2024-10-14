c1fa74b0f3e1ff022661d97540d75bed0273d91e
GearMasters <gearclubnewmexico@gmail.com>
2024-10-13 19:29:24 -0600

    Added sensor tests to HelloMotor and cleaned up DeepDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDrive.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-        import com.qualcomm.robotcore.hardware.CRServo;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "DeepDrive (Blocks to Java)")
 public class DeepDrive extends LinearOpMode {
@@ -35,23 +35,13 @@ public class DeepDrive extends LinearOpMode {
         BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
-        //rightarm = hardwareMap.get(DcMotor.class, "right arm");
-        //thingy1066 = hardwareMap.get(CRServo.class, "thingy -106.6");
-        //plane = hardwareMap.get(Servo.class, "plane");
         BackRight = hardwareMap.get(DcMotor.class, "BackRight");
-        //leftarm = hardwareMap.get(DcMotor.class, "left arm");
-        //intake = hardwareMap.get(DcMotor.class, "intake");
-        //thingy351 = hardwareMap.get(CRServo.class, "thingy 35.1");
 
         BackLeft.setDirection(DcMotor.Direction.REVERSE);
         FrontRight.setDirection(DcMotor.Direction.REVERSE);
         FrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        //rightarm.setDirection(DcMotor.Direction.REVERSE);
-        //thingy1066.setDirection(CRServo.Direction.REVERSE);
-        //plane.setPosition(1);
-        hex_motor_ticks = 288;
-        Right_Arm = 0;
-        Left_Arm = 0;
+
+
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
@@ -62,8 +52,8 @@ public class DeepDrive extends LinearOpMode {
                 BackRight.setPower((-pivot + vertical + horizontal) * 0.8);
                 FrontLeft.setPower((pivot + vertical + horizontal) * 0.8);
                 BackLeft.setPower((pivot + (vertical - horizontal)) * 0.8);
-                //leftarm.setPower(gamepad2.left_stick_y * 0.75);
-                //rightarm.setPower(gamepad2.left_stick_y * 0.75);
-                //intake.setPower(gamepad2.left_trigger);
-                //intake.setPower(-gamepad2.right_trigger);
-                BackLeft.setTargetPosition((int) 0.5);}}}}
+                BackLeft.setTargetPosition((int) 0.5);
+            }
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloMotor.java
@@ -2,17 +2,29 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp()
 public class HelloMotor extends OpMode {
 
     private DcMotor rightMotor;
+    private ColorSensor color;
+    private DistanceSensor distance;
+    private TouchSensor touchSen;
     @Override
     public void init()
     {
         rightMotor = hardwareMap.get(DcMotor.class, "FrontRight");
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        touchSen = hardwareMap.get(TouchSensor.class, "TouchSen");
+        color = hardwareMap.get(ColorSensor.class, "Color");
+        distance =  hardwareMap.get(DistanceSensor.class, "distance");
     }
 
     @Override
@@ -26,7 +38,16 @@ public class HelloMotor extends OpMode {
         {
             power = -gamepad1.right_trigger;
         }
+        if (touchSen.isPressed())
+        {
+            power = 0;
+        }
         rightMotor.setPower(power);
         telemetry.addData("power1", power);
+        telemetry.addData("Touched", touchSen.getValue());
+        telemetry.addData("Red", color.red());
+        telemetry.addData("Green", color.green());
+        telemetry.addData("Blue", color.blue());
+        telemetry.addData("Distance", distance.getDistance(DistanceUnit.CM));
     }
 }
