63c1b0d58618bf58806c89b2a4fe9637d930b417
GearMasters <gearclubnewmexico@gmail.com>
2024-11-03 16:38:55 -0700

    I may have fixed the driving program. I also have added color sensor code to deep drive and figured out how to update the telemetry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDrive.java
@@ -1,6 +1,12 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,10 +15,12 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp(name = "DeepDrive (Blocks to Java)")
 public class DeepDrive extends LinearOpMode {
-    private TouchSensor touchSen;
+
+    private TouchSensor TouchSen;
+    private ColorSensor color;
 
     private DcMotor BackLeft;
-  // private DcMotor FrontRight;
+   private DcMotor FrontRight;
     private DcMotor FrontLeft;
     //private DcMotor rightarm;
     //private CRServo thingy1066;
@@ -22,7 +30,7 @@ public class DeepDrive extends LinearOpMode {
     //private DcMotor intake;
     //private CRServo thingy351;
     private DcMotor Arm;
-    //private DcMotor Arm2;
+    private DcMotor Arm2;
 
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
@@ -40,16 +48,17 @@ public class DeepDrive extends LinearOpMode {
 
 
         BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
-       // FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
         BackRight = hardwareMap.get(DcMotor.class, "BackRight");
         Arm = hardwareMap.get(DcMotor.class, "Arm");
-     //   Arm2 = hardwareMap.get(DcMotor.class, "Arm2");
-        touchSen = hardwareMap.get(TouchSensor.class, "touchSen");
+        Arm2 = hardwareMap.get(DcMotor.class, "Arm2");
+        TouchSen = hardwareMap.get(TouchSensor.class, "TouchSen");
+        color = hardwareMap.get(ColorSensor.class, "Color");
 
-        //BackRight.setDirection(DcMotor.Direction.REVERSE);
-        //BackLeft.setDirection(DcMotor.Direction.REVERSE);
-      //  FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+       // BackLeft.setDirection(DcMotor.Direction.REVERSE);
+       FrontRight.setDirection(DcMotor.Direction.REVERSE);
         FrontLeft.setDirection(DcMotor.Direction.REVERSE);
 
 
@@ -70,42 +79,48 @@ public class DeepDrive extends LinearOpMode {
                     power = -gamepad1.right_trigger;
                     gamepad1.rumble(10000);
                 }
-                if (touchSen.isPressed())
-                {
-                    power = 0;
-                }
+                //if (touchSen.isPressed())
+               // {
+                 //   power = 0;
+               // }
 
 
 
 
                Arm.setPower(power);
 
-                if (gamepad1.left_trigger > 0) // if left trigger > 0
+                if (gamepad2.left_trigger > 0) // if left trigger > 0
                 {
-                    power2 = gamepad1.left_trigger;
-                    gamepad1.rumble(100);
+                    power2 = gamepad2.left_trigger;
+                    gamepad2.rumble(100);
                 }
                 else // check rtrigger
                 {
                     power2 = -gamepad2.right_trigger;
-                    gamepad1.rumble(100);
+                    gamepad2.rumble(100);
                 }
-                if (touchSen.isPressed())
+                if (TouchSen.isPressed())
                 {
                     power2 = 0;
                 }
-               // Arm2.setPower(power2);
+               Arm2.setPower(power2);
 
-                vertical = -gamepad1.right_stick_y;
-                horizontal = gamepad1.right_stick_x;
-                pivot = gamepad1.left_stick_x;
-              //  FrontRight.setPower((-pivot + (vertical - horizontal)) * 0.8);
+                vertical = -gamepad1.left_stick_y;
+                horizontal = gamepad1.left_stick_x;
+                pivot = gamepad1.right_stick_x;
+                FrontRight.setPower((-pivot + (vertical - horizontal)) * 0.8);
                 BackRight.setPower((-pivot + vertical + horizontal) * 0.8);
                 FrontLeft.setPower((pivot + vertical + horizontal) * 0.8);
                 BackLeft.setPower((pivot + (vertical - horizontal)) * 0.8);
                 BackLeft.setTargetPosition((int) 0.5);
                 telemetry.addData("power1", power);
-                telemetry.addData("Touched", touchSen.getValue());
+                telemetry.addData("Touched", TouchSen.getValue());
+                telemetry.addData("Red", color.red());
+                telemetry.addData("Green", color.green());
+                telemetry.addData("Blue", color.blue());
+                telemetry.update();
+
+
             }
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloMotor.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
