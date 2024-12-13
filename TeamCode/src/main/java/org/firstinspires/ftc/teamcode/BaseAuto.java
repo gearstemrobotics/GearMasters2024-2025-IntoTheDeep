afa51a443759656f2f18ca8985217472026502b5
GearMasters <gearclubnewmexico@gmail.com>
2024-12-12 19:15:15 -0700

    Created base auto class. I also added it to NewTestAutoEncode though I need to add it to all of my other autos.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAuto.java
@@ -0,0 +1,147 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+
+public abstract class BaseAuto extends LinearOpMode {
+
+    protected TouchSensor TouchSen;
+    protected RevBlinkinLedDriver blinkinLedDriver;
+    protected ColorSensor color;
+
+    protected CameraMonitor cameraMonitor;
+
+    protected DcMotor BackLeft;
+    protected DcMotor FrontRight;
+    protected DcMotor FrontLeft;
+    protected DcMotor BackRight;
+    protected DcMotor extendArm;
+    protected DcMotor liftArm;
+    protected DcMotor angleArm;
+
+
+    int anglePos;
+    int liftPos;
+    int extendPos;
+    int FrontRightPos;
+    int LeftArmPos;
+    int BackRightPos;
+    int RightArmPos;
+    int FrontLeftPos;
+    int BackLeftPos;
+
+    BaseAuto()
+    {
+        extendPos = 0;
+        liftPos = 0;
+        anglePos = 0;
+        FrontRightPos = 0;
+        BackRightPos = 0;
+        FrontLeftPos = 0;
+        BackLeftPos = 0;
+    }
+
+    //arm encoder stuff
+    protected void arm(double ExtendArmTarget, double LiftArmTarget, double AngleArmTarget, double Speed) {
+        extendPos += ExtendArmTarget;
+        liftPos += LiftArmTarget;
+        anglePos += AngleArmTarget;
+        extendArm.setTargetPosition(extendPos);
+        liftArm.setTargetPosition(liftPos);
+        angleArm.setTargetPosition(anglePos);
+        extendArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        angleArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        extendArm.setPower(Speed);
+        liftArm.setPower(Speed);
+        angleArm.setPower(Speed);
+        while (opModeIsActive() && (extendArm.isBusy() || liftArm.isBusy() || angleArm.isBusy())) {
+            // Do nothing
+        }
+        extendArm.setPower(0);
+        liftArm.setPower(0);
+        angleArm.setPower(0);
+    }
+
+    protected void drive(double FrontRightTarget, double BackRightTarget,
+                       double FrontLeftTarget, double BackLeftTarget, double Speed) {
+
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        FrontRightPos += FrontRightTarget;
+        BackRightPos += BackRightTarget;
+        FrontLeftPos += FrontLeftTarget;
+        BackLeftPos += BackLeftTarget;
+        FrontRight.setTargetPosition(FrontRightPos);
+        BackRight.setTargetPosition(BackRightPos);
+        FrontLeft.setTargetPosition(FrontLeftPos);
+        BackLeft.setTargetPosition(BackLeftPos);
+        FrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRight.setPower(Speed);
+        BackRight.setPower(Speed);
+        FrontLeft.setPower(Speed);
+        BackLeft.setPower(Speed);
+        while (opModeIsActive() && (FrontRight.isBusy() || BackRight.isBusy() || FrontLeft.isBusy() || BackLeft.isBusy())) {
+            // Do nothing
+        }
+        FrontRight.setPower(0);
+        BackRight.setPower(0);
+        FrontLeft.setPower(0);
+        BackLeft.setPower(0);
+    }
+
+    protected abstract void RunOpModeInnerLoop();
+
+    @Override
+    public void runOpMode() {
+
+         Map();
+
+        waitForStart();
+        if (opModeIsActive())
+        {
+            PrepMotor();
+            RunOpModeInnerLoop();
+        }
+    }
+
+    protected void Map() {
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "blinkinLedDriver");
+        BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        extendArm = hardwareMap.get(DcMotor.class, "extendArm");
+        liftArm = hardwareMap.get(DcMotor.class, "liftArm");
+        angleArm = hardwareMap.get(DcMotor.class, "angleArm");
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+    }
+
+    protected void PrepMotor() {
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        extendArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        angleArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        extendArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        angleArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+}
+
