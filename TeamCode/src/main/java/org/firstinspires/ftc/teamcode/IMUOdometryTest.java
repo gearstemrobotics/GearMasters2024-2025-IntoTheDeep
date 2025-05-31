640a07af5045d085b949ce7cf3bb475ae260e1db
GearMasters <gearclubnewmexico@gmail.com>
2025-05-31 13:55:58 -0600

    Added GoBilda examples for the pinpoint driver.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUOdometryTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.motors.GoBILDA5201Series;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class IMUOdometryTest extends LinearOpMode {
+    //public GoBildaPinpointDriver odo;
+    @Override public void runOpMode()
+    {
+        if (opModeIsActive())
+        {
+
+
+        }
+    }
+
+}
+
